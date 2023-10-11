f9fcb336d2e9e8bf4709c5ab46666cae539bcb6c
shivam.singh41 <shivam.singh41@CHNMCT331502L.ad.infosys.com>
2023-10-11 15:29:14 +0530

    Sixth Commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into user 
+(id,EMAIL_ADDRESS,FIRST_NAME,LAST_NAME,ROLE,SSN,USER_NAME)
+values(101, 'kreddy@stacksimplify.com', 'Kalyan', 'Reddy', 'admin','ssn101', 'kreddy');
+  insert into user 
+(id,EMAIL_ADDRESS,FIRST_NAME,LAST_NAME,ROLE,SSN,USER_NAME)
+values(102, 'gwiser@stacksimplify.com', 'Greg', 'Wiser', 'admin', 'ssn102', 'gwiser');
+insert into user 
+(id,EMAIL_ADDRESS,FIRST_NAME,LAST_NAME,ROLE,SSN,USER_NAME)
+ values(103, 'dmark@stacksimplify.com', 'David', 'Mark', 'admin', 'ssn103', 'dmark');  
